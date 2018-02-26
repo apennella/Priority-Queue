3fa1c772634529cdeec304b4f096199645df3d2b
Tony Pennella <apennella@albany.edu>
2018-02-26 14:17:15 -0500

    everything is working correctley just need to do the Json part

--- PriorityQueue.java
+++ PriorityQueue.java
@@ -10,55 +10,89 @@ public class PriorityQueue
 		newNode.job  = job;
 		newNode.next = null;
 		
+		// set the head node if it is not already set
 		if(head == null)
 		{
 			head = newNode;
 		}
 		
+		// set of statements to organize and set nodes other than the head node
 		else if (newNode.data > head.data)
 		{
+			// if the node after head is null sets head.next node to the new node
 			if (temp.next == null)
 			{
 				head.next = newNode;
-				//System.out.println(newNode.data);
 			}
+			// if head.next isn't null then checks for certain conditions to find where it goes in the queue
 			else
 			{
 				Node temp4 = temp;
 				int counter = 0;
+				
+				/* initiates the counter variable for the next loop. It navigates to where the node needs to go*/
 				while (temp4.data < newNode.data && temp4.next != null){
 					if (temp4.next != null){
 					temp4 = temp4.next;
 					counter++;
 					}
-					System.out.println(newNode.data);
 				}
-				
+				/* moves temp to the correct position*/
 				for (int i =0; i != counter-1; i++)
 				{
 					temp = temp.next;
 				}
-				
+				/* Used for setting a node that is not at the end of the queue*/
 				if (temp.next != null){
 					Node temp2 = temp.next;
 					temp.next = newNode;
 					temp = temp.next;
 					temp.next = temp2;
-					//System.out.println(newNode.data);
+					
 				}
-				
+				// Sets the last node if that is where the node goes
 				else {
-					//System.out.println(newNode.data);
+					
 					temp.next = newNode;
 				}
 			}
 		}
+		/* appends to the end of the list*/
 		else {
-		    //System.out.println("did it");
 			Node temp3 = head;
 			head = newNode;
 			head.next = temp3;
 		}
+		// checks the list to make sure its sorted and if it is not sorts it
+		sort(head);
+	}
+	public void dequeue() {
+		head = head.next;
+	}
+	
+	public void sort(Node head){
+		
+		Node temp = head;
+		Node check = temp.next;
+		
+		
+		while (temp.next != null){
+			
+			if (temp.data > check.data){
+				String job = check.job;
+				int data = check.data;
+				check.data = temp.data;
+				check.job = temp.job;
+				temp.data = data;
+				temp.job = job;
+			}
+			temp = temp.next;
+			if (check.next != null){
+				
+			check = check.next;
+			}
+		}
+
 	}
 	public void print()
 	{
@@ -68,8 +102,5 @@ public class PriorityQueue
 			temp = temp.next;
 		}
 		
-	}
-	
-
-	
+	}	
 }
--- Runner.java
+++ Runner.java
@@ -5,19 +5,15 @@ public class Runner {
 	
 	PriorityQueue queue = new PriorityQueue();
 	
+	queue.enqueue(0 , "job1");
+	queue.enqueue(1 , "job1");
+	queue.dequeue();
+	queue.enqueue(2, "job1");
+	queue.enqueue(3 , "job1");
 	queue.enqueue(4 , "job1");
-	queue.enqueue(4 , "job2");
-	queue.enqueue(4 , "job3");
-	queue.enqueue(3 , "job4");
-	queue.enqueue(6 , "job5");
-	queue.enqueue(4 , "job6");
-	queue.enqueue(3 , "job7");
-	queue.enqueue(5 , "job8");
-	queue.enqueue(7 , "job9");
-	queue.enqueue(9 , "job10");
-	queue.enqueue(5 , "job11");
-	queue.enqueue(8 , "job12");
-	queue.enqueue(10, "job13");
+	queue.enqueue(3 , "job2");
+	queue.dequeue();
+
 	queue.print();
  }
  
